6470e0c8f2d406ad685f14feee7c03ff3f8f70b1
kevinwforwork <kevinwforwork@163.com>
2016-05-30 09:36:10 +0800

    1 初始化控件
    1 初始化控件

---
+++ KWPageView/KWPageView/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  KWPageView
+//
+//  Created by 王鑫 on 16/5/30.
+//  Copyright © 2016年 KW. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ KWPageView/KWPageView/ViewController.m
@@ -0,0 +1,27 @@
+//
+//  ViewController.m
+//  KWPageView
+//
+//  Created by 王鑫 on 16/5/30.
+//  Copyright © 2016年 KW. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
